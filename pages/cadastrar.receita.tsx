d7354c72361093a8a4d7243e50ae8ad7dc374ac7
Cecilia Lourenco <ceciliamblourenco@gmail.com>
2022-11-09 19:43:22 -0300

    add new image upload system

--- pages/cadastrar.receita.tsx
+++ pages/cadastrar.receita.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/alt-text */
+import { useRef } from "react";
 import {
   Button,
   Col,
@@ -7,10 +9,81 @@ import {
   Row,
 } from "react-bootstrap";
 import Form from "react-bootstrap/Form";
+import { useForm, useFieldArray } from "react-hook-form";
 import Aba from "../components/Aba";
 import BarraNavegacao from "../components/BarraNavegacao";
+import { FaRegTrashAlt } from "react-icons/fa";
+import { FiUpload } from "react-icons/fi";
+import { useDropzone } from "react-dropzone";
 
 function CadastrarReceita() {
+  const formRef = useRef<HTMLFormElement | null>(null);
+  const { register, control } = useForm();
+
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "ingredients",
+  });
+  if (fields.length === 0) {
+    append({
+      name: "",
+      quantity: "",
+      measure: "",
+    });
+  }
+
+  function DropzoneWithoutDrag(_props: any) {
+    const { getRootProps, getInputProps, acceptedFiles } = useDropzone({
+      noDrag: true,
+    });
+    if (acceptedFiles.length > 0) {
+      const url = URL.createObjectURL(acceptedFiles[0]);
+      return (
+        // eslint-disable-next-line @next/next/no-img-element
+        <img
+          src={url}
+          style={{
+            height: "400px",
+          }}
+        />
+      );
+    }
+
+    return (
+      <section className="container">
+        <div {...getRootProps({ className: "dropzone" })}>
+          <input {...getInputProps()} />
+          <span>
+            <FiUpload
+              style={{
+                fontSize: "30",
+                color: "#FFF",
+                width: "100%",
+                height: "150px",
+                marginBottom: "1rem",
+                borderRadius: "0.3rem",
+                display: "flex",
+                justifyContent: "Center",
+                alignItems: "center",
+                cursor: "pointer",
+              }}
+            />
+          </span>
+          <p>Escolha uma foto para a sua receita</p>
+        </div>
+      </section>
+    );
+  }
+
+  function handleFormSubmit() {
+    if (!formRef.current) {
+      return false;
+    }
+    const formData = Object.fromEntries(
+      new FormData(formRef.current).entries()
+    );
+    console.log(formData);
+  }
   return (
     <Container>
       <Row>
@@ -19,65 +92,108 @@ function CadastrarReceita() {
           <BarraNavegacao />
           <div>
             <main>
-              <Form className="g-3">
-                <Form.Group className="mb-3" controlId="title">
+              <Form className="g-3" ref={formRef}>
+                <Form.Group className="mb-3">
                   <FormControl
                     type="text"
                     placeholder="Digite o título da receita"
+                    name="title"
                   />
                 </Form.Group>
 
-                <Form.Group className="mb-3" controlId="title">
+                <Form.Group className="mb-3">
                   <Form.Control
                     type="text"
                     placeholder="Digite a categoria (Exemplos: dieta, final de semana, lanche, sobremesa...)"
+                    name="category"
                   />
                 </Form.Group>
+                <div className="mb-3 p-2 text-center">
+                  <DropzoneWithoutDrag />
+                </div>
 
-                <Form.Group controlId="formFile" className="mb-3">
-                  <Form.Control type="file" />
-                </Form.Group>
+                <fieldset className="border border-success mb-3 p-2 ">
+                  {fields.map((ingredient, index) => {
+                    return (
+                      <div
+                        className="d-flex mb-3 justify-content-around"
+                        key={ingredient.id}
+                      >
+                        <Form.Control
+                          placeholder="Digite o nome do ingrediente"
+                          defaultValue={ingredient.name}
+                          className="me-5 p-2 w-75"
+                          {...register(`ingredients.${index}.name`)}
+                        />
+                        <InputGroup>
+                          <Form.Control
+                            placeholder="quantidade"
+                            defaultValue={ingredient.quantity}
+                            className="text-center"
+                            {...register(`ingredients.${index}.quantity`)}
+                          />
+                          <Form.Select
+                            aria-label="Default select example"
+                            defaultValue={ingredient.measure}
+                            {...register(`ingredients.${index}.measure`)}
+                          >
+                            <option selected>Escolha a medida</option>
+                            <option value={"xícara"}>xícara de chá</option>
+                            <option value={"colher de sopa"}>
+                              colher de sopa
+                            </option>
+                            <option value={"colher de sobremesa"}>
+                              colher de sopa
+                            </option>
+                            <option value={"colher de chá"}>
+                              colher de chá
+                            </option>
+                            <option value={"colher de café"}>
+                              colher de café
+                            </option>
+                            <option value={"ml"}>mililitros</option>
+                            <option value={"g"}>gramas</option>
+                            <option value={"unidade(s)"}>unidade(s)</option>
+                          </Form.Select>
+                        </InputGroup>
 
-                <Row className="mb-3">
-                  <Form.Group as={Col}>
-                    <Form.Control placeholder="Digite o nome do ingrediente" />
-                  </Form.Group>
-                  <Form.Group as={Col}>
-                    <Form.Control
-                      type="number"
-                      aria-label="Text input with dropdown button"
-                      placeholder="Quantidade"
-                    />
-                  </Form.Group>
-                  <Form.Group as={Col}>
-                    <Form.Select aria-label="Default select example">
-                      <option>Escolha a medida</option>
-                      <option value="1">xícara de chá</option>
-                      <option value="2">colher de sopa</option>
-                      <option value="3">colher de chá</option>
-                      <option value="4">colher de café</option>
-                      <option value="5">mililitros</option>
-                      <option value="6">gramas</option>
-                    </Form.Select>
-                  </Form.Group>
-                </Row>
-                <Button
-                  style={{ background: "#98D3DF" }}
-                  type="submit"
-                  className="mb-3"
-                >
-                  Adicionar
-                </Button>
+                        <a onClick={() => remove(index)}>
+                          <FaRegTrashAlt
+                            fontSize="1.5rem"
+                            className="ms-2"
+                            style={{
+                              color: "Tomato",
+                            }}
+                          />
+                        </a>
+                      </div>
+                    );
+                  })}
+
+                  <Button
+                    style={{ background: "#98D3DF" }}
+                    className="mb-3"
+                    onClick={() =>
+                      append({ quantity: null, name: null, measure: null })
+                    }
+                  >
+                    Adicionar ingrediente
+                  </Button>
+                </fieldset>
 
                 <Form.Group className="mb-3" controlId="instructions">
                   <Form.Control
                     as="textarea"
                     rows={5}
                     placeholder="Modo de preparo"
+                    name="instructions"
                   />
                 </Form.Group>
 
-                <Button style={{ background: "#98D3DF" }} type="submit">
+                <Button
+                  style={{ background: "#98D3DF" }}
+                  onClick={handleFormSubmit}
+                >
                   Cadastrar
                 </Button>
               </Form>
