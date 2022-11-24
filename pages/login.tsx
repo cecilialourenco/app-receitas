ad5079df1ab76aec9d6810501c831b3dc9b2cc68
Cecilia Lourenco <ceciliamblourenco@gmail.com>
2022-11-24 19:03:31 -0300

    update login page

--- pages/login.tsx
+++ pages/login.tsx
@@ -4,42 +4,110 @@ import { Col, Container, Row } from "react-bootstrap";
 import Aba from "../components/Aba";
 import BarraNavegacao from "../components/BarraNavegacao";
 import Link from "next/link";
+import { useState } from "react";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSubmit = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    setError("");
+
+    const user = {
+      email,
+      password,
+    };
+
+    console.log(user);
+  };
+
   return (
     <Container>
       <Row>
         <Col>
           <Aba />
           <BarraNavegacao />
-          <div>
-            <Form>
-              <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Email</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-                <Form.Text className="text-muted">
-                  Nunca iremos compartilhar o seu email.
-                </Form.Text>
-              </Form.Group>
-
-              <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Senha</Form.Label>
-                <Form.Control type="password" placeholder="Senha" />
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Lembrar-me" />
-              </Form.Group>
-              <Button
-                className="mb-3"
-                style={{ background: "#98D3DF" }}
-                type="submit"
+          <div style={{ textAlign: "center" }}>
+            <h1 style={{ color: "#67A438" }}>Faça o seu login</h1>
+            <form
+              onSubmit={handleSubmit}
+              style={{ maxWidth: "50%", margin: "0 auto" }}
+            >
+              <label
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  marginBottom: "1em",
+                  color: "#000",
+                }}
+              >
+                <span
+                  style={{
+                    marginBottom: "0.3em",
+                    fontWeight: "bold",
+                    textAlign: "left",
+                  }}
+                >
+                  E-mail:
+                </span>
+                <input
+                  type="email"
+                  name="email"
+                  required
+                  placeholder="E-mail do usuário"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </label>
+              <label
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  marginBottom: "1em",
+                  color: "#000",
+                }}
+              >
+                <span
+                  style={{
+                    marginBottom: "0.3em",
+                    fontWeight: "bold",
+                    textAlign: "left",
+                  }}
+                >
+                  Senha:
+                </span>
+                <input
+                  type="password"
+                  name="password"
+                  required
+                  placeholder="Insira sua senha"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </label>
+              <button
+                style={{
+                  backgroundColor: "#67A438",
+                  color: "#fff",
+                  textAlign: "center",
+                  cursor: "pointer",
+                  borderRadius: "10px",
+                  width: "120px",
+                  fontWeight: "bold",
+                  border: "none",
+                  padding: "10px 15px",
+                  fontSize: "1em",
+                }}
               >
                 Entrar
-              </Button>
-            </Form>
-            <Link href="/signup">
-              <a>Não possui uma conta? Cadastre-se</a>
-            </Link>
+              </button>
+              {"  "}
+              <Link href="/signin">
+                <a>Não possui uma conta? Cadastre-se</a>
+              </Link>
+            </form>
           </div>
         </Col>
       </Row>
