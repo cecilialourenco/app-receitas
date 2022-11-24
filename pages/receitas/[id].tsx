ebdbb564f0e0ce82235dfb3de713420b3f992e13
Cecilia Lourenco <ceciliamblourenco@gmail.com>
2022-11-24 19:03:05 -0300

    create id file in receitas folder

---
+++ pages/receitas/[id].tsx
@@ -0,0 +1,49 @@
+/* eslint-disable react/jsx-key */
+import { doc } from "firebase/firestore";
+import { useRouter } from "next/router";
+import { Col, Container, Row } from "react-bootstrap";
+import { useDocument } from "react-firebase-hooks/firestore";
+import Aba from "../../components/Aba";
+import BarraNavegacao from "../../components/BarraNavegacao";
+import Recipe from "../../components/Recipe";
+import { db } from "../../lib/firebase";
+
+function Receita() {
+  const router = useRouter();
+  const { id } = router.query;
+
+  if (!id) {
+    return <p>loading</p>;
+  }
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const [recipe, recipeLoading, recipeError] = useDocument(
+    doc(db, "Recipes", id as string),
+    {}
+  );
+
+  if (recipe) {
+    var data = recipe.data();
+    console.log(data);
+  }
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <Aba />
+          <BarraNavegacao />
+          {data && recipe && (
+            <Recipe
+              title={data.title}
+              cover={data.cover}
+              id={recipe.id}
+              instructions={data.instructions}
+              ingredients={data.ingredients}
+            />
+          )}
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default Receita;
