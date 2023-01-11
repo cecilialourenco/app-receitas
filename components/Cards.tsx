e9e8e15c8c42de7be9b16a02662e6c437ec7fffd
Cecilia Lourenco <ceciliamblourenco@gmail.com>
2023-01-11 11:39:26 -0300

    add search function in Cards

--- components/Cards.tsx
+++ components/Cards.tsx
@@ -4,7 +4,11 @@ import Card from "./Card";
 import { useCollection } from "react-firebase-hooks/firestore";
 import { Button, Spinner } from "react-bootstrap";
 
-function Cards() {
+type Props = {
+  search: string;
+};
+
+function Cards({ search }: Props) {
   const [recipes, recipesLoading, recipesError] = useCollection(
     collection(db, "Recipes"),
     {}
@@ -25,7 +29,10 @@ function Cards() {
   }
   if (recipes) {
     console.log(recipes);
-    cardList = recipes.docs.map((doc) => {
+    const visibleRecipes = recipes.docs.filter((recipe) =>
+      recipe.data().title.toLowerCase().includes(search.toLowerCase())
+    );
+    cardList = visibleRecipes.map((doc) => {
       const data = doc.data();
       return (
         <Card title={data.title} cover={data.cover} id={doc.id} key={doc.id} />
