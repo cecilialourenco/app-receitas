cd95a55675a7bd1491d7137936ce1a322cc9f10c
Cecilia Lourenco <ceciliamblourenco@gmail.com>
2023-01-11 11:39:41 -0300

    add modal component

--- components/DiaDaSemana.tsx
+++ components/DiaDaSemana.tsx
@@ -1,8 +1,18 @@
+import { useState } from "react";
+import { Button, Form, Modal } from "react-bootstrap";
+import Card from "./Card";
+import Cards from "./Cards";
+
 type Props = {
   dia: string;
 };
 
-const DiaDaSemana = ({ dia }: Props) => {
+function DiaDaSemana({ dia }: Props) {
+  const [showModal, setShowModal] = useState(false);
+
+  const closeModal = () => setShowModal(false);
+  const openModal = () => setShowModal(true);
+  const [searchText, setSearchText] = useState("");
   return (
     <div
       style={{
@@ -15,6 +25,7 @@ const DiaDaSemana = ({ dia }: Props) => {
       <div
         style={{
           display: "flex",
+          flexDirection: "column",
           justifyContent: "center",
           alignItems: "center",
           fontSize: "30px",
@@ -27,9 +38,61 @@ const DiaDaSemana = ({ dia }: Props) => {
         }}
       >
         {dia}
+
+        <Button
+          onClick={openModal}
+          style={{
+            backgroundColor: "transparent",
+            color: "white",
+            textAlign: "center",
+            cursor: "pointer",
+            border: "none",
+            fontSize: "30px",
+          }}
+        >
+          +
+        </Button>
+
+        <Modal
+          show={showModal}
+          onHide={closeModal}
+          size="lg"
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>Receitas</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form.Control
+              type="search"
+              placeholder="Digite o que você procura:"
+              className="mb-3"
+              aria-label="Search"
+              value={searchText}
+              onChange={(e) => setSearchText(e.target.value)}
+            />
+            <Cards search={searchText} />
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={closeModal}>
+              Fechar
+            </Button>
+            <Button variant="primary" onClick={closeModal}>
+              Adicionar
+            </Button>
+          </Modal.Footer>
+        </Modal>
       </div>
+      <div
+        style={{
+          width: "20%",
+          height: "100%",
+          display: "flex",
+        }}
+      ></div>
     </div>
   );
-};
+}
 
 export default DiaDaSemana;
