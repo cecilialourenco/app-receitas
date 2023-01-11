d93f9cd2a82ce113e511cc39369d457c38816d5e
Cecilia Lourenco <ceciliamblourenco@gmail.com>
2023-01-11 11:46:36 -0300

    add style to cover

--- components/TituloImagem.tsx
+++ components/TituloImagem.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
 import AreaBranca from "./AreaBranca";
 
 type Props = {
@@ -8,7 +10,12 @@ function TituloImagem({ title, cover }: Props) {
   return (
     <AreaBranca className="text-center">
       <h2>{title}</h2>
-      <img src={cover} width={400} height={300} />
+      <img
+        style={{ objectFit: "cover" }}
+        src={cover}
+        width={400}
+        height={300}
+      />
     </AreaBranca>
   );
 }
