a7445de9ddad643de6bbac30d7e42c898025e6a3
Cecilia Lourenco <ceciliamblourenco@gmail.com>
2023-12-04 16:08:38 -0300

    add meal categories

--- pages/weekly.planning.tsx
+++ pages/weekly.planning.tsx
@@ -7,7 +7,7 @@ export default function WeeklyPlanning() {
   return (
     <Layout>
       <h1
-        style={{
+        style={{  
           color: "#67A438",
           textAlign: "center",
         }}
@@ -15,9 +15,51 @@ export default function WeeklyPlanning() {
         Cardápio da Semana
       </h1>
       <p style={{ textAlign: "center", color: "#aaa" }}>
-        Adicione as refeições que vão fazer parte do cardápio da semana
+        Adicione as comidinhas que vão fazer parte do cardápio da semana
       </p>
+      
+      
       <div style={{ display: "flex", flexDirection: "column", gap: "20px" }}>
+        <div style={{fontSize: "20px", color:  "#67A438"}}>Selecione as categorias de refeições do seu dia a dia:</div>
+        <div 
+          className="btn-group" 
+          role="group" aria-label="Basic checkbox toggle button group">
+        <input type="checkbox" className="btn-check" id="btnCheck1" autoComplete="off"/>
+        <label 
+          className="btn btn-outline-primary" 
+          htmlFor="btnCheck1"
+        >CAFÉ DA MANHÃ</label>
+
+        <input type="checkbox" className="btn-check" id="btnCheck2" autoComplete="off"/>
+        <label 
+          className="btn btn-outline-primary" 
+          htmlFor="btnCheck2"
+        >COLAÇÃO</label>
+
+        <input type="checkbox" className="btn-check" id="btnCheck3" autoComplete="off"/>
+        <label 
+          className="btn btn-outline-primary" 
+          htmlFor="btnCheck3"
+        >ALMOÇO</label>
+
+        <input type="checkbox" className="btn-check" id="btnCheck4" autoComplete="off"/>
+        <label 
+          className="btn btn-outline-primary" 
+          htmlFor="btnCheck4"
+        >LANCHE</label>
+
+        <input type="checkbox" className="btn-check" id="btnCheck5" autoComplete="off"/>
+        <label 
+          className="btn btn-outline-primary" 
+          htmlFor="btnCheck5"
+        >JANTAR</label>
+
+        <input type="checkbox" className="btn-check" id="btnCheck6" autoComplete="off"/>
+        <label 
+          className="btn btn-outline-primary" 
+          htmlFor="btnCheck6"
+        >CEIA</label>
+      </div>
         <Weekdays dia={"Seg"} />
         <Weekdays dia={"Ter"} />
         <Weekdays dia={"Qua"} />
